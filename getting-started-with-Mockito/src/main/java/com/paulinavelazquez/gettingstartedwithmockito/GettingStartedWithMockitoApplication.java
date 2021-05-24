9daf7962c43027b3be0950cec7ea4fcb9e71708d
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-05-24 18:11:09 -0400

    Initial Commit

---
+++ getting-started-with-Mockito/src/main/java/com/paulinavelazquez/gettingstartedwithmockito/GettingStartedWithMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.paulinavelazquez.gettingstartedwithmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettingStartedWithMockitoApplication.class, args);
+    }
+
+}
---
+++ getting-started-with-Mockito/src/test/java/com/paulinavelazquez/gettingstartedwithmockito/GettingStartedWithMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulinavelazquez.gettingstartedwithmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
