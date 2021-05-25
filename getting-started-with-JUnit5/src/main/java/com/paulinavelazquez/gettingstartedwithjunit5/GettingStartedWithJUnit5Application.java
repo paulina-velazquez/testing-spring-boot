cf67d6aaa82f93a14ec8e6602969a2af67a12437
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-05-25 09:55:07 -0400

    Initial project add

---
+++ getting-started-with-JUnit5/src/main/java/com/paulinavelazquez/gettingstartedwithjunit5/GettingStartedWithJUnit5Application.java
@@ -0,0 +1,13 @@
+package com.paulinavelazquez.gettingstartedwithjunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithJUnit5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettingStartedWithJUnit5Application.class, args);
+    }
+
+}
---
+++ getting-started-with-JUnit5/src/test/java/com/paulinavelazquez/gettingstartedwithjunit5/GettingStartedWithJUnit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulinavelazquez.gettingstartedwithjunit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithJUnit5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
