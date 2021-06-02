7876112d573ede01c22926ea775716342b4a333b
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-06-02 18:05:26 -0400

    Add Parameterized Test Method Provider

--- advanced-junit-testing/src/test/java/com/paulinavelazquez/advancedjunittesting/model/OwnerTest.java
+++ advanced-junit-testing/src/test/java/com/paulinavelazquez/advancedjunittesting/model/OwnerTest.java
@@ -5,10 +5,9 @@ import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
@@ -74,5 +73,20 @@ class OwnerTest implements ModelTests {
     @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
     void csvFromFileTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    // Parameterized Methods Test
+    @DisplayName("Method Provider Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("getArgs")
+    void fromMethodTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    static Stream<Arguments> getArgs() {
+        return Stream.of(
+                Arguments.of("FL", 5, 1),
+                Arguments.of("OH", 2, 8),
+                Arguments.of("MI", 3, 5));
     }
 }
