f0c99fa57f3d4cfbe57f85ab2b85f04a35fd610b
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-06-02 16:53:23 -0400

    add Parameterized Test - CSV Source example

--- advanced-junit-testing/src/test/java/com/paulinavelazquez/advancedjunittesting/model/OwnerTest.java
+++ advanced-junit-testing/src/test/java/com/paulinavelazquez/advancedjunittesting/model/OwnerTest.java
@@ -5,6 +5,7 @@ import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -46,11 +47,23 @@ class OwnerTest implements ModelTests {
         System.out.println(val);
     }
 
-    // Enum Test
+    // Parameterized Enum Test
     @DisplayName("Enum Source Test")
     @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
     @EnumSource(OwnerType.class)
     void enumTest(OwnerType ownerType) {
         System.out.println(ownerType);
+    }
+
+    // Parameterized CSV
+    @DisplayName("CSV Input Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvSource({
+            "FL, 1, 1",
+            "OH, 2, 2",
+            "MI, 3, 1"
+    })
+    void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
     }
 }
