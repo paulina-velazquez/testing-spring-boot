c58d77ef249b6e98699fe3e30c090d82529b51b8
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-06-02 17:54:00 -0400

    add Parameterized Test - CSV File example

--- advanced-junit-testing/src/test/java/com/paulinavelazquez/advancedjunittesting/model/OwnerTest.java
+++ advanced-junit-testing/src/test/java/com/paulinavelazquez/advancedjunittesting/model/OwnerTest.java
@@ -5,6 +5,7 @@ import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -64,6 +65,14 @@ class OwnerTest implements ModelTests {
             "MI, 3, 1"
     })
     void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    // Parameterized CSV File
+    @DisplayName("CSV From File Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFromFileTest(String stateName, int val1, int val2) {
         System.out.println(stateName + " = " + val1 + ":" + val2);
     }
 }
