c89dfde032a75a82ad94a07dc589c67b1d767046
paulina_velazquez <mpaulina.velazquez@gmail.com>
2021-06-03 09:19:40 -0400

    add extension example

--- advanced-junit-testing/src/test/java/com/paulinavelazquez/advancedjunittesting/services/springdatajpa/PetTypeSDJpaServiceIT.java
+++ advanced-junit-testing/src/test/java/com/paulinavelazquez/advancedjunittesting/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -1,10 +1,13 @@
 package com.paulinavelazquez.advancedjunittesting.services.springdatajpa;
 
+import com.paulinavelazquez.advancedjunittesting.junitextensions.TimingExtensions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(TimingExtensions.class)
 class PetTypeSDJpaServiceIT {
 
     @BeforeEach
